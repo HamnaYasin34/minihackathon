02982c661d4f58ea1c949d10ad40d0d5feab6462
Hamna Yasin <130100572+HamnaYasin34@users.noreply.github.com>
2023-12-02 04:17:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ let thumbnails = document.querySelectorAll('.thumbnail .item');
 let countItem = items.length;
 let itemActive = 0;
 // event next click
-next.onclick = function(){
+next.onclick = function () {
     itemActive = itemActive + 1;
-    if(itemActive >= countItem){
+    if (itemActive >= countItem) {
         itemActive = 0;
     }
     showSlider();
 }
 //event prev click
-prev.onclick = function(){
+prev.onclick = function () {
     itemActive = itemActive - 1;
-    if(itemActive < 0){
+    if (itemActive < 0) {
         itemActive = countItem - 1;
     }
     showSlider();
@@ -26,7 +26,7 @@ prev.onclick = function(){
 let refreshInterval = setInterval(() => {
     next.click();
 }, 5000)
-function showSlider(){
+function showSlider() {
     // remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
     let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
@@ -59,60 +59,77 @@ function submitForm() {
     const email = document.getElementById("email").value;
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
-  
+
     let data;
     if (!name && !email && !subject && !message) {
-      // form validation through js
-      alert("please fill Out the form");
+        // form validation through js
+        alert("please fill Out the form");
     } else {
-      !name
-        ? alert("Name is required")
-        : !email
-        ? alert("Email is required")
-        : !subject
-        ? alert("Subject is required")
-        : !message
-        ? alert("Message is required")
-        : (data = { name, email, subject, message });
-      console.log(data);
-      resetForm();
+        !name
+            ? alert("Name is required")
+            : !email
+                ? alert("Email is required")
+                : !subject
+                    ? alert("Subject is required")
+                    : !message
+                        ? alert("Message is required")
+                        : (data = { name, email, subject, message });
+        console.log(data);
+        resetForm();
     }
-  
-  function resetForm() {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("subject").value = "";
-    document.getElementById("message").value = "";
-  }
- }
- 
- document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+    function resetForm() {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+    }
+}
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
     event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let subject = document.getElementById('subject').value;
     let message = document.getElementById('message').value;
-    
+
     // Process the form data here (e.g., send it to the server)
-    
+
     alert('Form submitted successfully!');
 });
 
 // read more read les
 
 // JavaScript function to toggle text content
-function toggleText(id, btn) {
-    const element = document.getElementById(id);
-    if (element.style.display === 'none' || element.style.display === '') {
-        element.style.display = 'block';
-        btn.textContent = 'Read Less';
-    } else {
-        element.style.display = 'none';
-        btn.textContent = 'Read More';
-    }
-}
+
+const btn = document
+    .querySelector('.read-more-btn');
+
+const text = document
+    .querySelector('.card__read-more');
+
+const cardHolder = document
+    .querySelector('.card-holder');
+
+cardHolder
+    .addEventListener('click', e => {
+
+        const current = e.target;
+
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+
+        if (!isReadMoreBtn)
+            return;
+
+        const currentText = e.target.parentNode.querySelector('.card__read-more');
+
+        currentText.classList.toggle('card__read-more--open');
+
+        current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'Read More...';
+
+    });
 
 
 
 
- 
+
